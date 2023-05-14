97dceb8329828debbe20308ba19ceb57c1b68c3b
SebasRodzSanz <133052102+SebasRodzSanz@users.noreply.github.com>
2023-05-13 20:48:01 -0600

    Correccion de las promesas

--- logica1.js
+++ logica1.js
@@ -7,65 +7,60 @@ formulario.addEventListener('submit',(e)=>{
 });
 
 const validar = function () {
-	const nombre=formulario.querySelector('#no_cuenta').value;
-	const contra=formulario.querySelector('#contra').value;
-
-	 const listo= validarCampos(nombre,contra)
-	 	.then(persona=>{
-	 		// console.log(persona.no_cuenta);
-	 		// console.log(persona.password);
-	 	})
-	 	.catch(err =>console.error('error en el sistema'));
-	
+		const nombre=formulario.querySelector('#no_cuenta').value;
+		const contra=formulario.querySelector('#contra').value;
+	 	const usuario= validarCampos(nombre,contra);
+	 	if (usuario===undefined) {
+	 		console.log('Los datos no fueron correctos');
+	 		//no se puede hacer una consulta a la base de datos
+	 	}else {
+	 		mensaje.innerHTML=`Tus datos se estan verificando...`;
+	 		//se puede hacer una consulta a la base de datos
+	 	}
 }
-
-const validarCampos = async (numero,pass)=>{
+const validarCampos = (numero,pass)=>{
 	try {
 		console.log("Inicio de validaciones");
 
-		let validado = await numeroValido(numero);
-		let espacio = await espaciosVacios(pass);
-		let cadena = await tamañoString(espacio);
+		let validado = numeroValido(numero);
+		let espacio = espaciosVacios(pass);
+		let cadena =  tamañoString(espacio);
 		console.log('Los datos estan listos para enviarse a la base de datos ');
-		const persona ={
+		const usuario ={
 			no_cuenta: validado,
-			password: cadena
+			password: cadena,
 		}
-		return new Promise ((resolve,reject)=>{resolve(persona)})
+		return usuario;
+
 	} catch(err) {
-		console.error(err);
+		console.error(err.message);
+		mensaje.innerHTML='Verifica el numero de cuenta o la contraseña';
 	}
 }
 function numeroValido(numero) {
 	let nuevo = parseInt(numero, 10);
 	if(Number.isNaN(nuevo)){
-		return Promise.reject('ERROR, No es un numero');
+		throw new Error ('Error, el dato ingresado no es un numero ');
+	}else {
+		return numero;
 	}
-	return new Promise((resolve,reject)=>{
-		resolve(nuevo);
-	})
+	
 }
 function tamañoString(pass) {
 	let n = pass.length;
-	if (n >12) {
-		return Promise.reject("ERROR, La contraseña no es valida");
-	}else if(n <=0){
-		return Promise.reject("ERROR, La contraseña no es valida");
+	if (n >8 || n <1) {
+		throw new Error('Error, numero de caracteres no valido');
 	}else if(n === 8){
-		return new Promise((resolve,reject)=>{
-			resolve(pass)
-		});
+		return pass;
 	}else {
-		return Promise.reject("ERROR, La contraseña no es valida: tamaño de la cadena");
+		throw new Error('Error, numero de caracteres no valido');
 	}
 }
 function espaciosVacios(pass) {
 	let array =pass.split(' ');
 	let n =array.length;
 	if(n >= 2){
-		return Promise.reject("ERROR, La contraseña no es valida: espacios en la cadena");
+		throw new Error('Error, la cadena contiene espacios en blanco');
 	}
-	return new Promise((resolve,reject)=>{
-			resolve(pass)
-		});
+	return pass;
 }
